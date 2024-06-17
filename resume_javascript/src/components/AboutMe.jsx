e2baae8234b1ec973cd0434484f08d313b7430b9
Juho Romppainen <juho.romppainen@tuni.fi>
2024-06-17 10:42:31 +0300

    bg image fixed

--- resume_javascript/src/components/AboutMe.jsx
+++ resume_javascript/src/components/AboutMe.jsx
@@ -1,6 +1,13 @@
+import mountainsImage from '../assets/icons/Mountains.jpg'
+
 const AboutMe = () => {
   return (
-    <div className='pt-80 bg-mountains bg-cover md:bg-center bg-right'>
+    <div
+      className='pt-80 bg-cover md:bg-center bg-right'
+      style={{
+        backgroundImage: `linear-gradient(to right, rgba(0,0,0,0.7), rgba(0,0,0,0)), url(${mountainsImage})`,
+      }}
+    >
       <div className='flex flex-col lg:w-1/3'>
         <h1 className='text-5xl md:text-8xl font-bold p-5 text-white'>
           Juho
